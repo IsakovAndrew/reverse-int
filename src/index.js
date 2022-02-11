4750f5658ae3d005e1f162188da0e8eb7dcede15
git config --global user.name <aibolit999999999@gmail.com>
2022-02-11 22:09:36 +0300

    fix: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    const reversed = n.toString().split('').reverse().join('')
+    if(n>=0){
+    return parseInt(reversed) * Math.sign(n)}
+    else return (parseInt(reversed)*Math.sign(n))* (-1)
 }
